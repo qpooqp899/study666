9f067f8d58b6a85332afb952d75a92e3c51651ff
qpooqp899 <qpooqp889@gmail.com>
2025-08-02 18:33:31 +0800

    Add files via upload

--- goldHunt/service-worker.js
+++ goldHunt/service-worker.js
@@ -1,21 +1,27 @@
 const CACHE_NAME = "adventure-game-cache-v1";
 const urlsToCache = [
-  "/",
   "/index.html",
   "/style.css",
   "/main.js",
   "/icon-192.png",
-  "/icon-512.png"
+  "/icon-512.png",
+  "/img"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }).catch((err) => {
+      console.error("🔴 Cache addAll failed:", err);
+    })
   );
 });
-
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
+
+
+
